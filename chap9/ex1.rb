f2cbb1458ab9c295eb80f824d95ea5c16ddce72c
ajdavey8 <ajdavey8@gmail.com>
2017-10-05 23:50:01 +0100

    Chapter9

---
+++ chap9/ex1.rb
@@ -0,0 +1,17 @@
+def ask question
+  while true
+    puts question
+    reply = gets.chomp.downcase
+      if reply == 'yes'
+        return true
+      end
+      if reply == 'no'
+        return false
+      end
+    puts 'Please answer "yes" or "no".'
+  end
+end
+
+ask 'Do you like apples?'
+like = ask 'Do you like pears?'
+puts like
